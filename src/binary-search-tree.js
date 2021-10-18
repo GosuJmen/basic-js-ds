4ba9b432b593e2f316633c2e05cbbd9019589438
Жерновой Евгений Николаевич <e.jernovoi@msu4.local>
2021-10-18 13:14:51 +0300

    feat: 1, 2 tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,55 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+
 module.exports = class BinarySearchTree {
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.data = null;
+    this.left = null;
+    this.right = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    if(this.data === null) {
+      return null
+    } else {
+      return this
+    }
+  }
+
+  add(data) {
+    if(this.data === null) {
+      this.data = data
+    } else if(this.data === data) {
+      return
+    } else if(this.data < data) {
+      this.left = whosNeht(this.left, data)
+    } else if(this.data > data) {
+      this.right = whosNeht(this.right, data)
+    } 
+    function whosNeht(node, data) {
+      if(!node) {
+        return new Node(data)
+      }
+
+      if(node.data === data) {
+        return null
+      }
+
+      if(data < node.data) {
+        node.left = whosNeht(node.left)
+      } else {
+        node.right = whosNeht(node.right, data)
+      }
+      return null
+    }
   }
 
   has(/* data */) {
